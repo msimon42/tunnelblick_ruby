52219b146ba220ce495c9f231ecf065ae4f1c455
msimon42 <msimon@uab.edu>
2021-08-09 16:03:18 -0500

    Add check connection method

--- lib/vpn_service.rb
+++ lib/vpn_service.rb
@@ -1,21 +1,14 @@
 class VpnService
-  attr_reader :address
-
-  def initialize
-    @address = nil
-  end
-  
-  def connect(address)
-      @address = address
-      system(
+  def self.connect(address)
+     system(
         %x{
             osascript <<APPLESCRIPT
             tell application "Tunnelblick"
-	             connect "#{@address}"
-	             get state of first configuration where name = "#{@address}"
+	             connect "#{address}"
+	             get state of first configuration where name = "#{address}"
 	             repeat until result = "CONNECTED"
 		              delay 1
-		              get state of first configuration where name = "#{@address}"
+		              get state of first configuration where name = "#{address}"
 	             end repeat
             end tell
             APPLESCRIPT}
@@ -24,8 +17,8 @@ class VpnService
     return "Connected to #{address}"
   end
 
-  def disconnect
-    raise 'No connection exists' unless @address
+  def self.disconnect
+    raise 'No connection exists' unless connected?
     system(
       %x{
           osascript <<APPLESCRIPT
@@ -38,10 +31,24 @@ class VpnService
     return 'Disconnected'
   end
 
-  def switch(address)
-    raise 'No connection exists' unless @address
+  def self.switch(address)
+    raise 'No connection exists' unless connected?
     self.disconnect
     self.connect(address)
     return "Connected to #{address}"
+  end
+
+  def self.connected?
+
+      output = `
+          osascript <<APPLESCRIPT
+          tell application "Tunnelblick"
+             get state of first configuration
+             return result
+          end tell
+          APPLESCRIPT
+      `
+
+    return output == "CONNECTED\n"
   end
 end
